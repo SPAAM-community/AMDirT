326550cdb355c2cc871fe72cacc54a08c88caeaf
maxibor <maxime.borry@gmail.com>
2020-08-25 18:12:59 +0200

    report all errors and reduce traceback

--- ancientMetagenomeDirCheck/main.py
+++ ancientMetagenomeDirCheck/main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import json
-from jsonschema import validate
+from jsonschema import Draft7Validator
 from io import StringIO
 from ancientMetagenomeDirCheck.exceptions import DatasetValidationError, DuplicateError
+import sys
 
 
 def check_validity(dataset, schema):
@@ -12,14 +13,29 @@ def check_validity(dataset, schema):
     with open(schema, "r") as j:
         json_schema = json.load(j)
 
-    validate(instance=dt_json, schema=json_schema)
+    v = Draft7Validator(json_schema)
+    errors = []
+    for error in sorted(v.iter_errors(dt_json), key=str):
+        errors.append(error)
+    if len(errors) > 0:
+        print("Validation Errors were found")
+        for error in errors:
+            err_column = list(error.path)[-1]
+            print("- ", error.message, f"in column {err_column}")
+        raise (DatasetValidationError("DatasetValidationError"))
+
 
 def check_duplicates(dataset):
     dt = pd.read_csv(dataset, sep="\t")
     if dt.duplicated().sum() != 0:
-        message = f"{dt[dt.duplicated()]} line is duplicated"
-        raise(DuplicateError(message))
+        message = f"Duplication Error\n{dt[dt.duplicated()]} line is duplicated"
+        raise (DuplicateError(message))
+
 
 def run_tests(dataset, schema):
-    check_validity(dataset, schema)
-    check_duplicates(dataset)
+    try:
+        check_duplicates(dataset)
+        check_validity(dataset, schema)
+    except (DatasetValidationError, DuplicateError) as e:
+        print(e)
+        sys.exit(1)
