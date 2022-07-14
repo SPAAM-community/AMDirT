e6d8744848d20cccb9601a1ff9352e1ce40b99db
James Fellows Yates <jfy133@gmail.com>
2022-07-14 10:08:27 +0200

    Remove all references to anthropogenic table as now removed

--- AMDirT/core/utils.py
+++ AMDirT/core/utils.py
@@ -134,7 +134,6 @@ def prepare_eager_table(samples, libraries, table_name, supported_archives):
 
     if table_name in [
         "ancientmetagenome-environmental",
-        "ancientmetagenome-anthropogenic",
     ]:
         sel_col = ["archive_accession"]
     else:
@@ -167,8 +166,6 @@ def prepare_eager_table(samples, libraries, table_name, supported_archives):
     selected_libraries["BAM"] = "NA"
     if table_name == "ancientmetagenome-environmental":
         selected_libraries["sample_host"] = "environmental"
-    elif table_name == "ancientmetagenome-anthropogenic":
-        selected_libraries["sample_host"] = "human"
     col2keep = [
         "sample_name",
         "archive_run_accession",
@@ -217,7 +214,6 @@ def prepare_accession_table(samples, libraries, table_name, supported_archives):
     )
     if table_name in [
         "ancientmetagenome-environmental",
-        "ancientmetagenome-anthropogenic",
     ]:
         sel_col = ["archive_accession"]
     else:
@@ -286,7 +282,6 @@ def is_merge_size_zero(samples, library, table_name):
 
     if table_name in [
         "ancientmetagenome-environmental",
-        "ancientmetagenome-anthropogenic",
     ]:
         sel_col = ["archive_accession"]
     else:
